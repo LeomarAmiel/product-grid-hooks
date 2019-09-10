82476d2aa970aa524446f238bda78928cc8c0d30
leomaramiel <leomaramieldev@gmail.com>
2019-09-10 13:20:28 +0800

    Fixed fetch hooks to handle scrolling when sorted

--- src/hooks/fetch.hooks.ts
+++ src/hooks/fetch.hooks.ts
@@ -22,30 +22,38 @@ interface Props {
 export function useFetch({ sort, page }: Props): IFetchValues {
   const [data, updateData] = useState([] as IProduct[]);
   const [isLoading, setIsLoading] = useState(false);
-  const hadSort = useRef(false);
+  const hadSort = useRef(sort);
 
   useEffect(() => {
+    // always show loading on first run of fetch
     setIsLoading(true);
-    if (sort === SORT_TYPES.UNSORTED && hadSort.current) {
+    if (sort === SORT_TYPES.UNSORTED && hadSort.current !== "") {
+      // from a sorted to unsorted
       fetch(`http://localhost:5000/products?_limit=${page * 20}`)
         .then(res =>
           res.json().then((newData: IProduct[]) => updateData(newData))
         )
         .finally(() => {
           setIsLoading(false);
-          hadSort.current = false;
+          hadSort.current = "";
         });
-    } else if (sort) {
+      // when changed sort
+    } else if (sort && hadSort.current !== sort) {
       fetch(`http://localhost:5000/products?_limit=${page * 20}&_sort=${sort}`)
         .then(res =>
           res.json().then((newData: IProduct[]) => updateData(newData))
         )
         .finally(() => {
           setIsLoading(false);
-          hadSort.current = true;
+          hadSort.current = sort;
         });
     } else {
-      fetch(`http://localhost:5000/products?_page=${page}&_limit=20`)
+      // handle only on scrolling
+      fetch(
+        `http://localhost:5000/products?_page=${page}&_limit=20${
+          sort ? `&_sort=${sort}` : ""
+        }`
+      )
         .then(res =>
           res
             .json()
